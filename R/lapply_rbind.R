52b47ae85e520c52ab2c8d1092af120409081cdf
RocArm <roc.armenter@gmail.com>
2018-02-19 12:27:15 -0500

    First version lapply_rbind.R

---
+++ R/lapply_rbind.R
@@ -0,0 +1,21 @@
+#' Lapply then rbind.
+#'
+#' A wrapper for a do.call('rbind', lapply(X, FUN)).
+#'
+#' @param X A list.
+#' @param FUN A function.
+#' @return A matrix or a data.frame, depending on the objects in X.
+#' @examples
+#' lapply_rbind( list(a = c(1,2), b =c(3,4)), sum)
+#' lapply_rbind( as.list(cars), function(x) mean(x))
+
+lapply_rbind <- function(X,
+                         FUN = function(x){x}){
+  # Checks
+  if (class(X)!='list') stop('X must be a list.')
+  if (class(FUN)!='function') stop('FUN must be a function.')
+
+  # Execute
+  list.output <- lapply(X, FUN)
+  return( do.call('rbind', list.output) )
+}
